a1db66d473ee3ef8aee509cc6006fbd71f4371aa
craigwann <craigwann@me.com>
2018-07-13 13:08:50 -0700

    pass the 2 .all tests

--- lib/project.rb
+++ lib/project.rb
@@ -15,6 +15,16 @@ class Project
     @id = result.first().fetch("id").to_i()
   end
 
+  def self.all
+    returned_projects = DB.exec("SELECT * FROM projects;")
+    projects = []
+    returned_projects.each() do |project|
+      title = project.fetch("title")
+      id = project.fetch("id").to_i()
+      projects.push(Project.new({:title => title, :id => id}))
+    end
+    projects
+  end
 
 
 
