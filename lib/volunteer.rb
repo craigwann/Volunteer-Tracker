fd4433f9f53f7ecb51033eaf6c25259056f1f304
craigwann <craigwann@me.com>
2018-07-13 22:59:23 -0700

    pass .find volunteer_spec (>_<)finally

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -15,18 +15,18 @@ class Volunteer
   def self.all
     returned_volunteers = DB.exec("SELECT * FROM volunteers;")
     volunteers = []
-    returned_volunteers.each() do |volunteer|
-      name = volunteer.fetch("name")
-      project_id = volunteer.fetch("project_id").to_i()
+    returned_volunteers.each do |volunteer|
+      name = volunteer.fetch('name')
+      project_id = volunteer.fetch('project_id').to_i
       id = volunteer.fetch('id').to_i
-      volunteers.push(Volunteer.new({:name => name, :project_id => project_id, :id =>nil}))
+      volunteers.push(Volunteer.new({:name => name, :project_id => project_id, :id =>id}))
     end
     volunteers
   end
 
   def save
     result = DB.exec("INSERT INTO volunteers (name, project_id) VALUES ('#{@name}', #{@project_id}) RETURNING id;")
-    @id = result.first().fetch("id").to_i()
+    @id = result.first().fetch('id').to_i
   end
 
   def self.find(id)
@@ -38,12 +38,5 @@ class Volunteer
     end
     found_volunteer
   end
-
-
-
-
-
-
-
 
 end
